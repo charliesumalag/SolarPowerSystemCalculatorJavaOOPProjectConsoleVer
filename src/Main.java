0eed65250a6b33c17e91d10ea6b41e68d64338e7
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-11-02 11:56:12 +0800

    System parameters

--- src/Main.java
+++ src/Main.java
@@ -10,37 +10,30 @@ public class Main {
 		
 		
 		System.out.println("-----------------------------------");
-			System.out.println("Solar Power System Calculator");
-			System.out.println("-----------------------------------");
-			int count = UserInput.getInt("How many appliances do you have? : ");
-			
-			for(int i = 1; i <= count; i++) {
-				System.out.println("\nAppliance " + i + ":");
-				String name = UserInput.getString("Enter appliance name: ");
-				double watts = UserInput.getDouble("Enter Wattage: ");
-				int qty = UserInput.getInt("Enter quantity: ");
-				int hoursPerDay = UserInput.getInt("Enter hours per day: ");
-				
-				
-				
-				calc.addAppliance(new Appliance(name, watts, qty, hoursPerDay));
-				
-				
-				
-			}
-			calc.viewAppliances();
-			
-			int PSH = UserInput.getInt("Enter Peak Sun Hours: ");
-			calc.setPSH(PSH);
+		System.out.println("Solar Power System Calculator");
+		System.out.println("-----------------------------------");
+		int count = UserInput.getInt("\nHow many appliances do you have : ");
+		
+		System.out.println("\nLoad Input");	
+		System.out.println("-----------------------------------");
+		for(int i = 1; i <= count; i++) {
+			System.out.println("Appliance " + "#" + i + ":");
+			String name = UserInput.getString("Name: ");
+			double watts = UserInput.getDouble("Wattage: ");
+			int qty = UserInput.getInt("Quantity: ");
+			int hoursPerDay = UserInput.getInt("Hours/Day: ");	
+			System.out.println("                            ");
+			calc.addAppliance(new Appliance(name, watts, qty, hoursPerDay));	
+		}
+		
+		calc.viewAppliances();
+		
+		System.out.println("\nSystem Parameters");	
+		System.out.println("-----------------------------------");
+		int PSH = UserInput.getInt("Enter Peak Sun Hours: ");
+		calc.setPSH(PSH);
 
 			
-			
-		
-			
-			
-			
-		
-		
 		
 
 	}
---
+++ src/SystemParameters.java
@@ -0,0 +1,21 @@
+
+public class SystemParameters {
+	private double PSH;
+	private double systemVoltage;
+	
+	
+	//creatieng contructor
+	public SystemParameters(double psh, double sysVol) {
+		this.PSH = psh;
+		this.systemVoltage = sysVol;
+	}
+	
+	//getter
+	public double getPSH(){ return PSH;}
+	
+	
+	
+	
+	
+	
+}
